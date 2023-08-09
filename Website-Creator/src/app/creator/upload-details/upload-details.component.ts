b8c3bf38e21bef41e7ab45219c711740fce9ae43
DESKTOP-CK234V8\adamt <adamtoda01@gmail.com>
2023-08-09 08:32:38 +0200

    load and upload file from firebase

---
+++ Website-Creator/src/app/creator/upload-details/upload-details.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FileUpload} from "../file-upload/file-upload";
+import {FileUploadService} from "../file-upload/file-upload.service";
+
+@Component({
+  selector: 'app-file-upload-details',
+  templateUrl: './upload-details.component.html',
+  styleUrls: ['./upload-details.component.css']
+})
+export class UploadDetailsComponent implements OnInit{
+  @Input() fileUpload!: FileUpload;
+
+  constructor(private uploadService: FileUploadService) { }
+
+  public ngOnInit(): void {
+  }
+
+  public deleteFileUpload(fileUpload: FileUpload): void {
+    this.uploadService.deleteFile(fileUpload);
+  }
+}
---
+++ Website-Creator/src/app/creator/upload-form/upload-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {FileUpload} from "../file-upload/file-upload";
+import {FileUploadService} from "../file-upload/file-upload.service";
+
+@Component({
+  selector: 'app-file-upload-form',
+  templateUrl: './upload-form.component.html',
+  styleUrls: ['./upload-form.component.css']
+})
+export class UploadFormComponent implements OnInit {
+  selectedFiles?: FileList;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
+
+  constructor(private uploadService: FileUploadService) { }
+
+  ngOnInit(): void {
+  }
+
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  public upload(): void {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+
+      if (file) {
+        this.currentFileUpload = new FileUpload(file);
+        this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(
+          percentage => {
+            this.percentage = Math.round(percentage ? percentage : 0);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      }
+    }
+  }
+}
